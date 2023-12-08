e643a63611115856dc65b00162d457ce0ea82554
JulioEvencio <julioigreja.dev@gmail.com>
2023-12-07 23:10:40 -0300

    added: implementing possible moves of king

--- chess/src/game/chess/pieces/King.java
+++ chess/src/game/chess/pieces/King.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import game.chess.Board;
 import game.chess.Color;
+import game.chess.Position;
 import game.resources.Spritesheet;
 
 public class King extends Piece {
@@ -24,7 +25,79 @@ public class King extends Piece {
 	public boolean[][] possibleMoves() {
 		boolean mat[][] = new boolean[super.getBoard().getROW()][super.getBoard().getCOLUMN()];
 
+		Position position = new Position(0, 0);
+
+		// Above
+		position.setRow(this.getPosition().getRow() - 1);
+		position.setColumn(this.getPosition().getColumn());
+
+		if (super.getBoard().positionExists(position) && this.canMove(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+
+		// Below
+		position.setRow(this.getPosition().getRow() + 1);
+		position.setColumn(this.getPosition().getColumn());
+
+		if (super.getBoard().positionExists(position) && this.canMove(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+
+		// Left
+		position.setRow(this.getPosition().getRow());
+		position.setColumn(this.getPosition().getColumn() - 1);
+
+		if (super.getBoard().positionExists(position) && this.canMove(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+
+		// Right
+		position.setRow(this.getPosition().getRow());
+		position.setColumn(this.getPosition().getColumn() + 1);
+
+		if (super.getBoard().positionExists(position) && this.canMove(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+
+		// NW
+		position.setRow(this.getPosition().getRow() - 1);
+		position.setColumn(this.getPosition().getColumn() - 1);
+
+		if (super.getBoard().positionExists(position) && this.canMove(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+
+		// NE
+		position.setRow(this.getPosition().getRow() - 1);
+		position.setColumn(this.getPosition().getColumn() + 1);
+
+		if (super.getBoard().positionExists(position) && this.canMove(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+
+		// SW
+		position.setRow(this.getPosition().getRow() + 1);
+		position.setColumn(this.getPosition().getColumn() - 1);
+
+		if (super.getBoard().positionExists(position) && this.canMove(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+
+		// SE
+		position.setRow(this.getPosition().getRow() + 1);
+		position.setColumn(this.getPosition().getColumn() + 1);
+
+		if (super.getBoard().positionExists(position) && this.canMove(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+
 		return mat;
+	}
+
+	private boolean canMove(Position position) {
+		Piece piece = super.getBoard().getPiece(position);
+
+		return piece == null || this.getColor() != piece.getColor();
 	}
 
 }
