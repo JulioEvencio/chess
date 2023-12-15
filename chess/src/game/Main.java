7fe82d0839bfa842ac83a06fe770a05ff934f653
JulioEvencio <julioigreja.dev@gmail.com>
2023-12-14 21:29:24 -0300

    added: SwingUtilities.invokeLater() invocation to ensure execution on EDT

--- chess/src/game/Main.java
+++ chess/src/game/Main.java
@@ -1,13 +1,17 @@
 package game;
 
+import javax.swing.SwingUtilities;
+
 import game.resources.Language;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Language.load();
+		SwingUtilities.invokeLater(() -> {
+			Language.load();
 
-		new Thread(new Game()).start();
+			new Thread(new Game()).start();
+		});
 	}
 
 }
